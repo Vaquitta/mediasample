9d3a8f7016b811dd52d99fbf4f99768fd9808fc9
Vaquitta <26276570+Vaquitta@users.noreply.github.com>
2018-01-01 14:16:03 +0000

    Initial
    Initial

---
+++ MediaSample/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.Integration;
+
+namespace MediaSample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ElementHost ctrlHost = new ElementHost();
+            patternPlayer.CustomPlayer customPlayer = new patternPlayer.CustomPlayer();
+
+            ctrlHost.Height = 250;
+            ctrlHost.Width = 350;
+
+            customPlayer.InitializeComponent();
+
+            ctrlHost.Child = customPlayer;
+          
+            this.panel1.Controls.Add(ctrlHost);
+
+            customPlayer.loadSource("");
+        }
+    }
+}
---
+++ patternPlayer/CustomPlayer.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace patternPlayer
+{
+    /// <summary>
+    /// Interaction logic for UserControl1.xaml
+    /// </summary>
+    public partial class CustomPlayer : UserControl
+    {
+        public CustomPlayer()
+        {
+            Unosquare.FFME.MediaElement.FFmpegDirectory = "C:\\ffmpeg\\";
+            InitializeComponent();
+            // InitializeMediaEvents();
+            //InitializeInputEvents();
+            //InitializeMainWindow();
+
+            //UpdateWindowTitle();
+           
+        }
+        public void loadSource(String src)
+        {
+            Media_Copy.Source = new Uri("file:///C:/ffmpegclientview/Gate_sample.avi");
+
+        }
+    }
+}
